b6b6d379e474adcb72b8926511c6b9410e3b829d
Wesley Mackey <wesmack96@gmail.com>
2022-11-08 11:14:15 -0600

    Get random computer selection

---
+++ game.js
@@ -0,0 +1,6 @@
+function getComputerChoice(compSelection) {
+    const gameChoices = ["Rock", "Paper", "Scissors"];
+    compSelection = gameChoices[Math.floor(Math.random()*gameChoices.length)];
+    return compSelection;
+}
+console.log(getComputerChoice());
